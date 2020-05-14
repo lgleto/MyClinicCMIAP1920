a362b2e10dfd34bdec1fe44c9e4d83aba5953109
Lourenço Gomes <leto.lg@gmail.com>
2020-05-14 11:06:09 +0100

    - get all medicos

--- app/src/main/java/ipca/example/myclinic/MainActivity.kt
+++ app/src/main/java/ipca/example/myclinic/MainActivity.kt
@@ -2,12 +2,18 @@ package ipca.example.myclinic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        VolleyHelper.instance.getAllMedicos(this) { response ->
+            response?.let {
+                Log.d("MainActivity", it.toString())
+            }
+        }
     }
 
 }
--- app/src/main/java/ipca/example/myclinic/VolleyHelper.kt
+++ app/src/main/java/ipca/example/myclinic/VolleyHelper.kt
@@ -9,6 +9,7 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.jetbrains.anko.doAsync
+import org.json.JSONArray
 import org.json.JSONObject
 import java.nio.charset.Charset
 
@@ -31,7 +32,7 @@ class VolleyHelper {
             jsonObject.put("utilizador", userName)
             jsonObject.put("password", password)
 
-            val jsonObjectRequest = object : JsonObjectRequest(
+            val jsonObjectRequest = JsonObjectRequest(
                 Request.Method.POST,
                 BASE_API + USER_LOGIN,
                 jsonObject,
@@ -52,17 +53,42 @@ class VolleyHelper {
                     // login mal sucedido
                     loginEvent.invoke(false)
                 }
-            ){
-
-            }
+            )
             queue!!.add(jsonObjectRequest)
         }
     }
+
+    fun getAllMedicos(context: Context, medicosEvent : ((JSONArray?)->Unit)){
+        doAsync {
+            queue = Volley.newRequestQueue(context)
+
+            val stringRequest = object : StringRequest(
+                Request.Method.GET,
+                BASE_API + MEDICOS,
+                Response.Listener<String>{
+                    medicosEvent.invoke(JSONArray(it))
+                },Response.ErrorListener {
+                    Log.d("VolleyHelper", it.toString())
+                    medicosEvent.invoke(null)
+                }
+            ){
+                override fun getHeaders(): MutableMap<String, String> {
+                    val map : MutableMap<String, String> = mutableMapOf<String, String>()
+                    map.put("x-access-token", token)
+                    return map
+                }
+            }
+
+            queue!!.add(stringRequest)
+        }
+    }
+
 
     companion object {
 
         const val  BASE_API = "http://89.153.72.138:3000"
         const val  USER_LOGIN = "/user/login"
+        const val  MEDICOS = "/api/medicos"
 
         var token = ""
 
